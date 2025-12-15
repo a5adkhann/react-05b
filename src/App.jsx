114e9f86822ea9deaa1e81576e7ed489e93f5fe8
FAIZ ALI <faizalioffical355@gmail.com>
2025-12-15 10:54:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Testing from "./features/Testing";
+
+function App(){
+  return(
+    <div>
+      
+      My Main Parent Component
+
+      <br />
+
+      <Testing/>
+      
+      </div>
+  )
+}
+
+export default App;
