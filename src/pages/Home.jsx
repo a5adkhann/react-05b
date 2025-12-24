64161c36b7ac75e85a39539a6266e4e67646f1ea
FAIZ ALI <faizalioffical355@gmail.com>
2025-12-24 10:54:50 +0500

    ---

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Cards from '../features/Cards'
+import Slideshow from '../features/Slideshow'
 
 const Home = () => {
   return (
     <>
+      <Slideshow/>
       <Cards/>
     </>
   )
