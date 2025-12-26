c52a65f918ca0554f3aa07bd9ec026d25a33f744
FAIZ ALI <faizalioffical355@gmail.com>
2025-12-26 10:40:48 +0500

    ---

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import { Menu } from 'lucide-react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
+
   return (
     <>
         <div className='navbar flex bg-linear-to-r from-blue-900 to-black p-4 text-white justify-between'>
             <div>Logo</div>
-            <div>
+            <div className='md:block hidden'>
               <ul className='flex gap-10'>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
@@ -14,6 +19,17 @@ const Navbar = () => {
                 <li><Link to="/contact">Contact</Link></li>
               </ul>
             </div>
+
+            <div className='md:hidden block' onClick={() => setToggle(!toggle)}>
+              <Menu />
+            </div>
+        </div>
+
+        <div className={` ${toggle ? "block" : "hidden"}  mobile-navbar bg-linear-to-r from-blue-900 to-black text-white text-center`}>
+          <p>Home</p>
+          <p>About</p>
+          <p>Services</p>
+          <p>Contact</p>
         </div>
     </>
   )
